4f587ba78084ee552cb1d0119e9ff84f0ebd2376
Maxwell Aboagye <nanabaah12@gmail.com>
2023-12-20 21:03:16 +0100

    Created Fake Data

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,11 +1,9 @@
-import "reflect-metadata"
-import {DataSource} from "typeorm"
-import dotenv from "dotenv";
+import "reflect-metadata";
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { runSeeders, SeederOptions } from 'typeorm-extension';
 
-dotenv.config()
-
-
-export const AppDataSource = new DataSource({
+// Define options for DataSource and Seeder
+const options: DataSourceOptions & SeederOptions = {
     type: process.env.DB_TYPE as any,
     host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT),
@@ -18,4 +16,40 @@ export const AppDataSource = new DataSource({
     migrations: ["src/migrations/**/*.ts"],
     subscribers: ["src/subscriber/**/*.ts"],
 
-})
+    seeds: ['src/seeds/**/*{.ts,.js}'],
+    factories: ['src/factories/**/*{.ts,.js}']
+};
+
+// Create AppDataSource instance
+export const AppDataSource = new DataSource(options);
+
+// Initialize AppDataSource and run seeders
+(async () => {
+    try {
+        await AppDataSource.initialize();
+        await runSeeders(AppDataSource);
+        console.log("Data Source has been initialized and seeders have been run!");
+    } catch (error) {
+        console.error("Error during Data Source initialization:", error);
+    }
+})();
+
+
+// const options: DataSourceOptions & SeederOptions = {
+//     type: process.env.DB_TYPE as any,
+//     host: process.env.DB_HOST,
+//     port: Number(process.env.DB_PORT),
+//     username: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     synchronize: true,
+//     logging: false,
+//     entities: ["src/entities/**/*.ts"],
+//     migrations: ["src/migrations/**/*.ts"],
+//     subscribers: ["src/subscriber/**/*.ts"],
+//
+//     seeds: ['src/seeds/**/*{.ts,.js}'],
+//     factories: ['src/factories/**/*{.ts,.js}']
+// };
+//
+// export const AppDataSource = new DataSource(options);
--- src/factory/comment.factory.ts
+++
@@ -1,9 +0,0 @@
-import {Faker} from '@faker-js/faker';
-import {setSeederFactory} from 'typeorm-extension';
-import Comment from '../entities/Comment';
-
-export const CommentsFactory = setSeederFactory(Comment, (faker: Faker) => {
-    const comment = new Comment();
-    comment.body = faker.lorem.sentences();
-    return comment;
-});
